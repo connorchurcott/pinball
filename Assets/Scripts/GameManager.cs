4aacbb9317ae969601a8c6e0e5490f0b2a3ac8fe
connorchurcott <connorchurcott@gmail.com>
2026-05-31 22:06:58 -0700

    added timer, target score, and scaling level difficulty

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,6 +7,8 @@ public class GameManager : MonoBehaviour
 
     [Header("Game Settings")]
     public int startingBalls = 2; 
+    public int targetScore = 10000; 
+    public float startingTime = 240f; 
 
     [Header("References (UI)")]
     public GameObject gameOverScreen; 
@@ -21,6 +23,11 @@ public class GameManager : MonoBehaviour
     public int Score { get; private set; }
     public int BallsRemaining { get; private set; }
     public bool IsGameOver { get; private set; }
+    public int CurrentScreen { get; private set; }
+
+    float timeRemaining; 
+    bool timerRunning; 
+    GameObject currentBall; 
 
     void Awake()
     {
@@ -35,13 +42,47 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
+        // set not game over
         IsGameOver = false; 
         gameOverScreen.SetActive(false); 
 
+        // set starting balls
         BallsRemaining = startingBalls; 
         infoManager.UpdateBallsUI(BallsRemaining);
-        SpawnBallInLauncher(); 
 
+        // set score, target score 
+        Score = 0; 
+        infoManager.UpdateScoreUI(Score); 
+        infoManager.UpdateTargetScoreUI(targetScore); 
+
+        // set time
+        timerRunning = true; 
+        timeRemaining = startingTime; 
+        infoManager.UpdateTimerUI(timeRemaining);
+
+        // set current screen and spawn first ball
+        CurrentScreen = 1; 
+        SpawnBallInLauncher(); 
+    }
+
+    void Update()
+    {
+        // don't run the timer if its not supposed to be running, like if the games paused
+        if (!timerRunning)
+        {
+            return; 
+        }
+
+        // Constantly update time, and check if its less than 0, if so then end the game 
+        timeRemaining -= Time.deltaTime; 
+        infoManager.UpdateTimerUI(timeRemaining);
+
+        if(timeRemaining <= 0)
+        {
+            timeRemaining = 0; 
+            infoManager.UpdateTimerUI(timeRemaining); 
+            TriggerGameOver(); 
+        }
     }
 
     public void OnBallDrained(GameObject ballDrained)
@@ -65,12 +106,43 @@ public class GameManager : MonoBehaviour
     {
         Score += Mathf.RoundToInt(points); 
         infoManager.UpdateScoreUI(Score); 
+        CheckWinCondition(); 
+    }
+
+    void CheckWinCondition()
+    {
+        if(Score >= targetScore)
+        {
+            LoadNextScreen(); 
+        }
+    }
+
+    void LoadNextScreen()
+    {
+        timerRunning = false; 
+        CurrentScreen++; 
+        Debug.Log("Screen Cleared, load next screen"); 
+
+        // add screen transition + resetting for next screen (hopefully soon :p)
+        // for now just increase target and keep playing 
+        targetScore += 5000 * CurrentScreen; 
+        infoManager.UpdateTargetScoreUI(targetScore);
+        timeRemaining = startingTime - 10 * CurrentScreen;  
+        infoManager.UpdateTimerUI(timeRemaining); 
+
+        if (currentBall)
+        {
+            Destroy(currentBall); 
+        }
+        SpawnBallInLauncher(); 
+        timerRunning = true; 
     }
 
     // creates new ball, and tells launcher that there is a new ball so it can updates it's ballRB and bar settings. 
     public void SpawnBallInLauncher()
     {
         GameObject newBall = Instantiate(ballPrefab, ballSpawnPosition.position, Quaternion.identity);
+        currentBall = newBall; 
         Rigidbody2D newBallRB = newBall.GetComponent<Rigidbody2D>(); 
         ballLauncher.SetBall(newBallRB); 
     }
@@ -78,6 +150,7 @@ public class GameManager : MonoBehaviour
     public void TriggerGameOver()
     {
         IsGameOver = true; 
+        timerRunning = false; 
         gameOverScreen.SetActive(true); 
     }
 
--- Assets/Scripts/InfoManager.cs
+++ Assets/Scripts/InfoManager.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using TMPro;
 using UnityEngine;
 
@@ -6,9 +7,18 @@ public class InfoManager : MonoBehaviour
     public int score { get; private set; }
 
     [Header("UI Reference")]
+    public TMP_Text timerText; 
     public TMP_Text scoreText;
     public TMP_Text ballsText; 
+    public TMP_Text targetScoreText; 
+    
 
+    public void UpdateTimerUI(float timeRemaining)
+    {
+        int minutes = Mathf.FloorToInt(timeRemaining / 60f); 
+        int seconds = Mathf.FloorToInt(timeRemaining % 60f); 
+        timerText.text = string.Format("{0}:{1:00}", minutes, seconds); 
+    }
 
     public void UpdateScoreUI(int newScore)
     {
@@ -16,6 +26,11 @@ public class InfoManager : MonoBehaviour
         {
             scoreText.text = "SCORE: " + newScore; 
         }
+    }
+
+    public void UpdateTargetScoreUI(int newTargetScore)
+    {
+        targetScoreText.text = "TARGET: " + newTargetScore; 
     }
 
     public void UpdateBallsUI(int BallsRemaining)
